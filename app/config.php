2f3979a9c04f65c0684b850092772b5e109079f9
Guilherme Garcia <guilhermeantonio337@gmail.com>
2026-03-12 19:16:24 -0300

    fix database name for railway

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', getenv('MYSQLHOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('MYSQLPORT') ?: '3306');
-define('DB_NAME', 'sirb_db');
+define('DB_NAME', getenv('MYSQLDATABASE') ?: 'railway');
 define('DB_USER', getenv('MYSQLUSER') ?: 'root');
 define('DB_PASS', getenv('MYSQLPASSWORD') ?: '');
 define('BASE_URL', '');
