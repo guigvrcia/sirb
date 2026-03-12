7c1344d6633c691e7445824b7270d95ea8a37f12
Guilherme Garcia <guilhermeantonio337@gmail.com>
2026-03-12 16:21:58 -0300

    Config banco Railway

--- app/config.php
+++ app/config.php
@@ -1,8 +1,6 @@
 <?php
-// Ajuste se seu MySQL tiver senha
-<?php
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'sirb_db');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: '');
-define('BASE_URL', getenv('BASE_URL') ?: ''); // pasta do projeto no htdocs
+
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASS', getenv('MYSQLPASSWORD'));
